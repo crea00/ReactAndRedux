f64e1dbdf8e7f4f7b4ccc7b04d9e725ddb4af520
crea00 <654321su@gmail.com>
2018-08-19 21:05:09 +0900

    #057 Ajax Requests with Axios

--- weather/src/actions/index.js
+++ weather/src/actions/index.js
@@ -1,1 +1,21 @@
+import axios from 'axios';
+
 const API_KEY = '5e286905e3491460810a6d81140e1079';
+const ROOT_URL = `https://api.openweathermap.org/data/2.5/forecast?appid=${API_KEY}`;
+
+export const FETCH_WETHER = 'FETCH_WETHER';
+
+// The Action creators always have to return an action 
+// & an action is an object which always has to have a type.
+export function fetchWeather(city) {
+  const url = `${ROOT_URL}&q=${city},us`;
+  // This will return a promise right here
+  const request = axios.get(url);
+
+  return {
+    type: FETCH_WETHER,
+    // The payload is an optional property that goes along with actions 
+    // that can contain some additional data that describes this particular action.
+    payload: request
+  };
+}
