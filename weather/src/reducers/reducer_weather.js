2cf24c10e1cc29ba1eb824b9d7a143434bd62caf
crea00 <654321su@gmail.com>
2018-08-21 22:34:32 +0900

    #062 Mapping Props to a Render Helper

--- weather/src/reducers/reducer_weather.js
+++ weather/src/reducers/reducer_weather.js
@@ -2,7 +2,7 @@ import { FETCH_WEATHER } from '../actions/index';
 
 export default function (state = [], action) {
   switch (action.type) {
-    case 'FETCH_WETHER':
+    case FETCH_WEATHER:
       // These two lines are nearly completely identical.
       // return state.concat([action.payload.data]);
       return [action.payload.data, ...state]; // [ city, city, city ] NOT [ city, [ city, city ] ]
